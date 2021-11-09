af2121c710434ef8d1270691157017d15f564c37
Ranadeep Biswas <mail@rnbguy.at>
2021-11-09 13:24:48 +0100

    propagate errors from Rust to Go

--- third_party/mbt/src/lib.h
+++ third_party/mbt/src/lib.h
@@ -1,2 +1,7 @@
-char *generate_json_traces_from_tla_tests_rs(char *tla_tests_file_path_c,
-                                             char *tla_config_file_path_c);
+typedef struct CResult {
+  char *json;
+  char *error;
+} CResult;
+
+struct CResult generate_json_traces_from_tla_tests_rs(char *tla_tests_file_path_c,
+                                                      char *tla_config_file_path_c);
--- third_party/mbt/src/lib.rs
+++ third_party/mbt/src/lib.rs
@@ -5,37 +5,47 @@ use std::os::raw::c_char;
 pub fn generate_json_traces_from_tla_tests(
     tla_tests_file_path: &str,
     tla_config_file_path: &str,
-) -> String {
+) -> Result<String, anyhow::Error> {
     let runtime = modelator::ModelatorRuntime::default();
-    let trace_results = runtime
-        .traces(tla_tests_file_path, tla_config_file_path)
-        .unwrap();
+    let trace_results = runtime.traces(tla_tests_file_path, tla_config_file_path)?;
     let trace_results = trace_results
         .into_iter()
-        .map(|(testname, traces)| {
-            let traces = traces.unwrap();
-            (
+        .map::<Result<_, anyhow::Error>, _>(|(testname, traces)| {
+            let traces = traces?;
+            Ok((
                 testname,
                 traces
                     .into_iter()
                     .map(|trace| trace.into_iter().collect::<Vec<_>>())
                     .collect::<Vec<_>>(),
-            )
+            ))
         })
-        .collect::<BTreeMap<_, _>>();
-    serde_json::to_string_pretty(&trace_results).unwrap()
+        .collect::<Result<BTreeMap<_, _>, _>>()?;
+    Ok(serde_json::to_string_pretty(&trace_results)?)
+}
+
+#[repr(C)]
+pub struct CResult {
+    json: *mut c_char,
+    error: *mut c_char,
 }
 
 #[no_mangle]
 pub extern "C" fn generate_json_traces_from_tla_tests_rs(
     tla_tests_file_path_c: *mut c_char,
     tla_config_file_path_c: *mut c_char,
-) -> *mut c_char {
+) -> CResult {
     let tla_tests_file_path = unsafe { CString::from_raw(tla_tests_file_path_c) };
     let tla_config_file_path = unsafe { CString::from_raw(tla_config_file_path_c) };
-    let json_string = generate_json_traces_from_tla_tests(
+    let (json_string, error_string) = match generate_json_traces_from_tla_tests(
         tla_tests_file_path.to_str().unwrap(),
         tla_config_file_path.to_str().unwrap(),
-    );
-    CString::new(json_string).unwrap().into_raw()
+    ) {
+        Ok(json) => (json, String::new()),
+        Err(error) => (String::new(), format!("{:?}", error)),
+    };
+    CResult {
+        json: CString::new(json_string).unwrap().into_raw(),
+        error: CString::new(error_string).unwrap().into_raw(),
+    }
 }
